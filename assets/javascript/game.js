42fd176c0f4be01a45a12e2a9cd2c173219e7176
Maldrek <scott.salem@gmail.com>
2018-02-05 14:40:40 -0600

    made initial changes and added pseudocode to js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Creates an array of numbers that can be used as the target number
+//Computer chooses a number from that array
+//Creates an array of numbers 1-12 that are to be assigned to the crystals
+//Sets a value of 1-12 (chosen randomly) for each image. Cannot equal another value assigned.
+//Creates a counter that tracks the values of each image.
+//When user clicks an image, the value of that image increases the counter by that value.
+//If the values clicked add up to === counter value, then win message appears and game resets!
+//If the values clicked > counter value, then loss message appears and game resets.
